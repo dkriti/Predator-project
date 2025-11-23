e88255cae87663787df3a84f885dedd96cd7159e
Divya Kriti <divyakriti1110@gmail.com>
2025-11-22 16:17:55 -0800

    Update run_featurecounts_cer_job.sh

--- run_featurecounts_cer_job.sh
+++ run_featurecounts_cer_job.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+cd $SLURM_SUBMIT_DIR
+
 # Define input/output
-OUT_FILE="/rnaseq_project/new_annotation/mapping_independent/featurecounts_T_cer_stringent/featurecounts_results_T_cer_stringent/gene_counts_T_cer_stringent.txt"
+OUT_FILE="/rnaseq_project/new_annotation/mapping_independent/featurecounts_cerevisiae/featurecounts_results/gene_counts_cerevisiae.txt"
 GTF="/rnaseq_project/new_annotation/genome/Saccharomyces_cerevisiae.R64-1-1.114.gtf"
 
 # Setup working directories
-cd /rnaseq_project/new_annotation/mapping_independent/featurecounts_T_cer_stringent
+cd /rnaseq_project/new_annotation/mapping_independent/featurecounts_cerevisiae
 
-mkdir -p featurecounts_results_T_cer_stringent
+mkdir -p featurecounts_results
 mkdir -p logs
 
 echo "Running featureCounts..."
