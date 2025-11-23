16ed3de83b68b53a258188630e6dd6ce8af5badd
Divya Kriti <divyakriti1110@gmail.com>
2025-11-22 16:16:51 -0800

    Update run_featurecounts_sch_job.sh

--- run_featurecounts_sch_job.sh
+++ run_featurecounts_sch_job.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name="featurecounts_T_sch_stringent_rnaseq"
-#SBATCH --account=st-cnislow-1
-#SBATCH --time=01:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=64G
-#SBATCH --output=%x-%j.log
-#SBATCH --error=logs/%x_%j.err
 
 cd $SLURM_SUBMIT_DIR
 
-module load zlib-ng/2.0.7
-module load gcc/9.4.0
-module load r/4.4.0
-module load miniconda3/4.9.2
-module load samtools
-source activate variant_env
-
-# Define input/output
-OUT_FILE="/scratch/st-cnislow-1/divya_scratch/rnaseq_project/new_annotation/mapping_independent/featurecounts_T_sch_stringent/featurecounts_results_T_sch_stringent/gene_counts_T_sch_stringent.txt"
-GTF="/scratch/st-cnislow-1/divya_scratch/rnaseq_project/new_annotation/genome/schoenii_annotated_final.gtf"
+# Define input/output for schoenii
+OUT_FILE="/rnaseq_project/new_annotation/mapping_independent/featurecounts_schoenii/featurecounts_results/gene_counts_schoenii.txt"
+GTF="/rnaseq_project/new_annotation/genome/schoenii_annotation.gtf"
 
 # Setup working directories
-cd /scratch/st-cnislow-1/divya_scratch/rnaseq_project/new_annotation/mapping_independent/featurecounts_T_sch_stringent
+cd /rnaseq_project/new_annotation/mapping_independent/featurecounts_schoenii
 
-mkdir -p featurecounts_results_T_sch_stringent
+mkdir -p featurecounts_results
 mkdir -p logs
 
 echo "Running featureCounts..."
